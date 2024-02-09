6c54852633f8b2ee82b7d37004f7a69780b1f936
pipnic99 <nicomunozinfantes1999@gmail.com>
2024-02-09 18:39:59 +0100

    cambio menu + inclusion nuevo hud

--- Assets/Scripts/HUD/CantidadCuchillos.cs
+++ Assets/Scripts/HUD/CantidadCuchillos.cs
@@ -6,11 +6,40 @@ using UnityEngine.UI;
 public class CantidadCuchillos : MonoBehaviour
 {
     public int numeroCuchillos = 0;
+    private GameObject[] hijos;
 
-    public Text textoVariable;
+    void Start()
+    {
+        // Obtén todos los GameObjects hijos de este GameObject, excluyendo este GameObject mismo
+        Transform[] hijosTransform = transform.GetComponentsInChildren<Transform>(true);
+
+        // Crea un array para almacenar solo los GameObjects hijos
+        hijos = new GameObject[hijosTransform.Length - 1];
+
+        // Itera sobre los GameObjects hijos y omite el GameObject actual
+        int index = 0;
+        foreach (Transform hijoTransform in hijosTransform)
+        {
+            if (hijoTransform.gameObject != gameObject)
+            {
+                hijos[index] = hijoTransform.gameObject;
+                index++;
+            }
+        }
+
+        // Ahora el array "hijos" contiene todos los GameObjects hijos, excluyendo este GameObject
+        // Puedes hacer lo que quieras con este array
+    }
     // Update is called once per frame
     void Update()
     {
-        textoVariable.text = "X " + numeroCuchillos.ToString();
+        for (int i = 0; i < numeroCuchillos && i < hijos.Length; i++)
+        {
+            hijos[i].SetActive(true);
+        }
+        for (int i = numeroCuchillos; i < hijos.Length; i++)
+        {
+            hijos[i].SetActive(false);
+        }
     }
 }
--- Assets/Scripts/KnifePickup.cs
+++ Assets/Scripts/KnifePickup.cs
@@ -8,11 +8,11 @@ public class KnifePickup : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Marcamos un condicionante para asegurarnos de que solo se active cuando el otro objeto tiene la etiqueta Player.
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && cantidadCuchillos.numeroCuchillos < 4)
         {
             // Ponemos el valor de viendoJugador a true.
             cantidadCuchillos.numeroCuchillos += 1;
-            Destroy(this.gameObject);
+            this.gameObject.SetActive(false);
         }
     }
 }
